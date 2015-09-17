ba0365897ab8230732707df25d3bda3e2ee67861
Minh Tri <minhtri@iMac-Minh.local>
2015-09-16 18:21:45 -0700

    change

---
+++ Emobile/Emobile/SeventhViewController.swift
@@ -0,0 +1,24 @@
+
+import UIKit
+
+class SeventhViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func shouldAutorotate() -> Bool {
+        return false
+    }
+    
+    override func supportedInterfaceOrientations() -> Int{
+        return UIInterfaceOrientation.Portrait.rawValue
+    }
+}
