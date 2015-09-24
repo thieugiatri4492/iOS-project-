2b8d9e354a32a2d2096ef645dbf5b53da6ed37fe
Zoom Nguyen <dung.nguyen@rainmaker-labs.com>
2015-09-24 14:49:31 +0700

    Fixed auto layout Login

--- Emobile/Emobile/ViewController.swift
+++ Emobile/Emobile/ViewController.swift
@@ -5,14 +5,14 @@ class ViewController: UIViewController {
 
     
 
-    @IBOutlet weak var username: CustomTextField!
+    @IBOutlet weak var username: UITextField!
 
-    @IBOutlet weak var password: CustomTextField!
+    @IBOutlet weak var password: UITextField!
     
     @IBOutlet weak var loginbutton: UIButton!
     var imageView: UIImageView!;
    
-    let bgImage = UIImage(named: "loginbg.png");
+//    let bgImage = UIImage(named: "loginbg.png");
     
     @IBAction func loginPress(sender: UIButton){
         self.username.resignFirstResponder();
@@ -26,24 +26,24 @@ class ViewController: UIViewController {
     override func  viewDidLoad() {
         super.viewDidLoad();
         
-        username.setId("username");
-        username.setImage();
-        password.setImage();
+//        username.setId("username");
+//        username.setImage();
+//        password.setImage();
         
         //background image handle
         
-       imageView = UIImageView(frame: view.bounds);
-        imageView.contentMode = .ScaleAspectFill;
-        imageView.clipsToBounds = true;
-        imageView.image = bgImage;
-        imageView.center = CGPointMake(view.bounds.midX, view.bounds.midY);
-       
-        
-        imageView.autoresizingMask = UIViewAutoresizing.FlexibleRightMargin | UIViewAutoresizing.FlexibleLeftMargin | UIViewAutoresizing.FlexibleBottomMargin | UIViewAutoresizing.FlexibleTopMargin | UIViewAutoresizing.FlexibleHeight | UIViewAutoresizing.FlexibleWidth;
-        
-        
-        view.addSubview(imageView);
-        self.view.sendSubviewToBack(imageView);
+//       imageView = UIImageView(frame: view.bounds);
+//        imageView.contentMode = .ScaleAspectFill;
+//        imageView.clipsToBounds = true;
+//        imageView.image = bgImage;
+//        imageView.center = CGPointMake(view.bounds.midX, view.bounds.midY);
+//       
+//        
+//        imageView.autoresizingMask = UIViewAutoresizing.FlexibleRightMargin | UIViewAutoresizing.FlexibleLeftMargin | UIViewAutoresizing.FlexibleBottomMargin | UIViewAutoresizing.FlexibleTopMargin | UIViewAutoresizing.FlexibleHeight | UIViewAutoresizing.FlexibleWidth;
+//        
+//        
+//        view.addSubview(imageView);
+//        self.view.sendSubviewToBack(imageView);
         
         
         
