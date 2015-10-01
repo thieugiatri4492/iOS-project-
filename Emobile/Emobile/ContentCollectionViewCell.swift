8fb370fad9f38ab9799fdd8185e1f9082d66d0ae
Minh Tri <minhtri@iMac-Minh.local>
2015-10-01 10:21:04 +0700

    new commit

---
+++ Emobile/Emobile/ContentCollectionViewCell.swift
@@ -0,0 +1,9 @@
+//
+//  ContentCollectionViewCell.swift
+//  Emobile
+//
+//  Created by Minh Tri on 9/30/15.
+//  Copyright (c) 2015 Minh Tri. All rights reserved.
+//
+
+import Foundation
---
+++ Emobile/Emobile/CustomCollectionLayout.swift
@@ -0,0 +1,9 @@
+//
+//  CustomCollectionLayout.swift
+//  Emobile
+//
+//  Created by Minh Tri on 9/29/15.
+//  Copyright (c) 2015 Minh Tri. All rights reserved.
+//
+
+import Foundation
