84b0e78bebe98a337eef5de80ec913b06ab5125f
Minh Tri <minhtri@iMac-Minh.local>
2015-10-05 11:12:33 +0700

    Update service

---
+++ Emobile/Emobile/CellMiniTableView.swift
@@ -0,0 +1,24 @@
+//
+//  CellMiniTableView.swift
+//  Emobile
+//
+//  Created by Minh Tri on 10/1/15.
+//  Copyright (c) 2015 Minh Tri. All rights reserved.
+//
+
+import UIKit
+
+class CellMiniTableView: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Emobile/Emobile/Service.swift
@@ -0,0 +1,9 @@
+//
+//  Service.swift
+//  Emobile
+//
+//  Created by Minh Tri on 10/2/15.
+//  Copyright (c) 2015 Minh Tri. All rights reserved.
+//
+
+import Foundation
